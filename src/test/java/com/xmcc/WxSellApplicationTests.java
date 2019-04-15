738407cace491352a7eece51c0fb2b352a5dce11
xmcc <2734704529@qq.com>
2019-04-15 16:47:21 +0800

    第一次测试：测试1

---
+++ src/test/java/com/xmcc/WxSellApplicationTests.java
@@ -0,0 +1,17 @@
+package com.xmcc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)//spring整合jiunit测试需要的东西
+@SpringBootTest
+public class WxSellApplicationTests {
+
+	@Test
+	public void contextLoads() {
+
+	}
+
+}
